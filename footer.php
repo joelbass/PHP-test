feafa4984dff8117b97964b7de89bfd5f345991f
joelbass <joelbass@gmail.com>
2015-10-08 19:29:14 -0600

    hey

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+  <!-- Our Site Navigation -->
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="contact.html">Contact</a>
+  </nav>
+
+  <!-- Site Copyright -->
+    <p>Copyright 2015 - Joel Bass and Friends</p>
+</footer>
+
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <!-- Adding the Page Title from the index, about, or contact pages -->
-    <title>Home | Your Name Here</title>
 
-    <!-- Tell the browser to render the view width correctly -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Link our stylesheet -->
-    <link rel="stylesheet" href="style.css">
-
-  </head>
-
-  <body id="home">
-    <header>
-  <div class="row clearfix">
-
-    <!-- Our Logo -->
-    <a href="index.html"><img class="logo" src="img/logo2.png"/></a>
-
-    <!-- Our Site Navigation -->
-    <nav>
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a href="contact.html">Contact</a>
-    </nav>
-
-  </div>
-</header>
 
 <!-- Our site's hero area -->
 <div class="hero">
